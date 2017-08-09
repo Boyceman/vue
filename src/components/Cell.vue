75b90d1b5c4a76e8f2fce4e67039f6a324e599d2
boyce <bcliu@wind.com.cn>
2017-08-09 16:14:31 +0800

    latestMeeting cells scroll when overflow

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,22 +1,58 @@
 <template>
-  <mt-cell class="Cell" :title="cell.title">
-    <img slot="icon" :src="cell.image">
-  </mt-cell>
+  <div class="Cell">
+    <img class="image" :src="image">
+    <div class="text">
+      <h2 class="title">{{ title }}</h2>
+      <p class="date">{{ date }}</p>
+      <span class="name">{{ name }}</span>
+      <span class="organization">{{ organization }}</span>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'Cell',
-  props: ['cell'],
-  data () {
-    return {
+  export default {
+    name: 'Cell',
+    props: ['cell'],
+    data () {
+      const { image, title, date, name, organization } = this.$props.cell
+      return {
+        image, title, date, name, organization
+      }
     }
   }
-}
 </script>
 
 <style scoped lang="scss" rel="stylesheet/scss" type="text/scss">
-  .Cell {
+  @import "../style/functions";
 
+  .Cell {
+    overflow: hidden;
+    padding: p2r(30) p2r(35);
+    background: #fff;
+    border: 1px solid #eee;
+    border-left: none;
+    border-right: none;
+    margin-bottom: p2r(10);
+    .image {
+      float: left;
+      margin-right: p2r(35);
+    }
+    .text {
+      text-align: left;
+      .title {
+        font-size: p2r(30);
+        line-height: 1;
+        color: #333333;
+        height: p2r(30);
+        @include Ellipsis(auto);
+        margin-bottom: p2r(22);
+        line-height: 1;
+      }
+      p, span {
+        font-size: p2r(20);
+        color: #999999;
+      }
+    }
   }
 </style>
--- src/routes/LatestMeeting.vue
+++ src/routes/LatestMeeting.vue
@@ -21,8 +21,60 @@
       return {
         cells: [
           {
-            title: 'hello 3c',
-            image: 'http://placehold.it/100x100'
+            title: '医药工业景气度回升，提早工业吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧',
+            image: 'http://placehold.it/56x56',
+            date: '2017-04-19（周二）10:00-11:00',
+            name: '杨德龙（宏观研究主管）',
+            organization: '前海开源'
+          },
+          {
+            title: '医药工业景气度回升，提早工业吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧',
+            image: 'http://placehold.it/56x56',
+            date: '2017-04-19（周二）10:00-11:00',
+            name: '杨德龙（宏观研究主管）',
+            organization: '前海开源'
+          },
+          {
+            title: '医药工业景气度回升，提早工业吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧',
+            image: 'http://placehold.it/56x56',
+            date: '2017-04-19（周二）10:00-11:00',
+            name: '杨德龙（宏观研究主管）',
+            organization: '前海开源'
+          },
+          {
+            title: '医药工业景气度回升，提早工业吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧',
+            image: 'http://placehold.it/56x56',
+            date: '2017-04-19（周二）10:00-11:00',
+            name: '杨德龙（宏观研究主管）',
+            organization: '前海开源'
+          },
+          {
+            title: '医药工业景气度回升，提早工业吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧',
+            image: 'http://placehold.it/56x56',
+            date: '2017-04-19（周二）10:00-11:00',
+            name: '杨德龙（宏观研究主管）',
+            organization: '前海开源'
+          },
+          {
+            title: '医药工业景气度回升，提早工业吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧',
+            image: 'http://placehold.it/56x56',
+            date: '2017-04-19（周二）10:00-11:00',
+            name: '杨德龙（宏观研究主管）',
+            organization: '前海开源'
+          },
+          {
+            title: '医药工业景气度回升，提早工业吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧',
+            image: 'http://placehold.it/56x56',
+            date: '2017-04-19（周二）10:00-11:00',
+            name: '杨德龙（宏观研究主管）',
+            organization: '前海开源'
+          },
+          {
+            title: '医药工业景气度回升，提早工业吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧啦吧',
+            image: 'http://placehold.it/56x56',
+            date: '2017-04-19（周二）10:00-11:00',
+            name: '杨德龙（宏观研究主管）',
+            organization: '前海开源'
           }
         ]
       }
@@ -31,7 +83,11 @@
 </script>
 
 <style scoped lang="scss" rel="stylesheet/scss" type="text/scss">
-  .LatestMeeting {
+  @import "../style/functions";
 
+  .LatestMeeting {
+    max-height: p2r(1116);
+    padding-top: p2r(10);
+    overflow: scroll;
   }
 </style>
